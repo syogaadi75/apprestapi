f4cd2b060ba895421bbf0f5eebd2161a8ad4abf8
Yoga <syogaadi75@gmail.com>
2020-09-13 18:17:28 +0700

    ganti server

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 // Parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// panggil router
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
