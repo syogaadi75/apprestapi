c10776db46a1a0c5b5ca0cc26df7d33dedf00bd9
Yoga <syogaadi75@gmail.com>
2020-09-14 01:11:35 +0700

    Get Data

--- controller.js
+++ controller.js
@@ -5,3 +5,14 @@
  exports.index = function (req,res) {
      response.ok("Aplikasi REST API berjalan!",res);
  }
+
+// Menampilkan semua data mahasiswa
+exports.tampilSemuaDataMahasiswa = function (req,res) {
+    connection.query('SELECT * FROM mahasiswa',function(error, rows, fields){
+        if(error){
+            connection.log(error)
+        } else {
+            response.ok(rows,res)
+        }
+    })
+}
